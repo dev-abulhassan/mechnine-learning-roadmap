cedea2993c15eb8332e0cc3bafe90a2ce226e1da
masudrana5 <abdullainfo30@gmail.com>
2023-08-02 22:52:53 +0600

    node is working

--- src/components/Flow.jsx
+++ src/components/Flow.jsx
@@ -1,39 +1,58 @@
-import React from "react";
-
+import React, { useCallback } from "react";
 import ReactFlow, {
-  addEdge,
   MiniMap,
   Controls,
   Background,
+  addEdge,
   useNodesState,
   useEdgesState,
 } from "reactflow";
+import {
+  nodes as initialNodes,
+  edges as initialEdges,
+} from "./initial-elements";
+
+const onInit = (reactFlowInstance) =>
+  console.log("flow loaded:", reactFlowInstance);
 import "reactflow/dist/style.css";
 
 const Flow = () => {
+  const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
+  const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
+  const onConnect = useCallback(
+    (params) => setEdges((eds) => addEdge(params, eds)),
+    [setEdges]
+  );
   return (
-    <>
-      <ReactFlow>
-        <MiniMap
-          nodeStrokeColor={(n) => {
-            if (n.style?.background) return n.style.background;
-            if (n.type === "input") return "#0041d0";
-            if (n.type === "output") return "#ff0072";
-            if (n.type === "default") return "#1a192b";
+    <ReactFlow
+      nodes={nodes}
+      edges={edges}
+      onNodesChange={onNodesChange}
+      onEdgesChange={onEdgesChange}
+      onConnect={onConnect}
+      onInit={onInit}
+      fitView
+      attributionPosition="top-right"
+    >
+      <MiniMap
+        nodeStrokeColor={(n) => {
+          if (n.style?.background) return n.style.background;
+          if (n.type === "input") return "#0041d0";
+          if (n.type === "output") return "#ff0072";
+          if (n.type === "default") return "#1a192b";
 
-            return "#eee";
-          }}
-          nodeColor={(n) => {
-            if (n.style?.background) return n.style.background;
+          return "#eee";
+        }}
+        nodeColor={(n) => {
+          if (n.style?.background) return n.style.background;
 
-            return "#fff";
-          }}
-          nodeBorderRadius={2}
-        />
-        <Controls />
-        <Background color="#aaa" gap={16} />
-      </ReactFlow>
-    </>
+          return "#fff";
+        }}
+        nodeBorderRadius={2}
+      />
+      <Controls />
+      <Background color="#aaa" gap={16} />
+    </ReactFlow>
   );
 };
 
